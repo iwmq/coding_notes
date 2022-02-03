5c1d2353713baba032095439fe1eaa1d9f133d0f
jaywang <iwmq@qq.com>
2022-02-03 19:37:22 +0800

    add go generic example

---
+++ go/generics/main.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+type Number interface {
+	int64 | float64
+}
+
+// SumInts adds together the values of m.
+func SumInts(m map[string]int64) int64 {
+	var s int64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumFloats adds together the values of m.
+func SumFloats(m map[string]float64) float64 {
+	var s float64
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+// SumIntsOrFloats adds together the values of m.
+func SumIntsOrFloats[T comparable, V int64 | float64](m map[T]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+
+// SumNumbers adds together the values of m.
+func SumNumbers[T comparable, V Number](m map[T]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	// Initialize a map for the integer values
+	ints := map[string]int64{
+		"first":  34,
+		"Second": 12,
+	}
+
+	// Initialize a map for the float64 values
+	floats := map[string]float64{
+		"first":  23.12,
+		"second": 87.33,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n",
+		SumInts(ints),
+		SumFloats(floats))
+
+	fmt.Printf("Generic Sums: %v and %v\n",
+		SumIntsOrFloats[string, int64](ints),
+		SumIntsOrFloats[string, float64](floats))
+
+	fmt.Printf("Generic Sums, type parameters inferred: %v and %v\n",
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
+}
